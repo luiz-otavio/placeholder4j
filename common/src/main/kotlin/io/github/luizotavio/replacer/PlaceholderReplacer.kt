16bd8f83e673e09d809596d9e2075950e5723712
luiz-otavio <luizfarias2006@hotmail.com>
2021-12-26 23:10:29 -0300

    refactor: replace variable name instead of placeholder

--- common/src/main/kotlin/io/github/luizotavio/replacer/PlaceholderReplacer.kt
+++ common/src/main/kotlin/io/github/luizotavio/replacer/PlaceholderReplacer.kt
@@ -1,17 +1,16 @@
 package io.github.luizotavio.replacer
 
 import io.github.luizotavio.exception.InternalPlaceholderException
-import kotlin.jvm.Throws
 
 interface PlaceholderReplacer<T> {
 
     @Throws(InternalPlaceholderException::class)
-    fun replace(placeholder: String, value: T? = null): String
+    fun replace(text: String, value: T? = null): String
 
     @Throws(InternalPlaceholderException::class)
     fun replace(collection: Collection<String>, value: T? = null): Collection<String>
 
     @Throws(InternalPlaceholderException::class)
-    fun replace(vararg collection: String, value: T? = null): Collection<String>
+    fun replace(vararg collection: String, value: T? = null): Array<String>
 
 }

4d63287d077f923c0ab8bc426dae1f0d03e55586
luiz-otavio <luizfarias2006@hotmail.com>
2021-12-26 21:34:17 -0300

    update(!): use InternalPlaceholderException into PlaceholderReplacer methods to throw anonymous exceptions

--- common/src/main/kotlin/io/github/luizotavio/replacer/PlaceholderReplacer.kt
+++ common/src/main/kotlin/io/github/luizotavio/replacer/PlaceholderReplacer.kt
@@ -1,11 +1,17 @@
 package io.github.luizotavio.replacer
 
+import io.github.luizotavio.exception.InternalPlaceholderException
+import kotlin.jvm.Throws
+
 interface PlaceholderReplacer<T> {
 
+    @Throws(InternalPlaceholderException::class)
     fun replace(placeholder: String, value: T? = null): String
 
+    @Throws(InternalPlaceholderException::class)
     fun replace(collection: Collection<String>, value: T? = null): Collection<String>
 
+    @Throws(InternalPlaceholderException::class)
     fun replace(vararg collection: String, value: T? = null): Collection<String>
 
 }
